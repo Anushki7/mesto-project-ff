b1ed10ccf9bf5fc7ba06ba6b52e48c72643062ba
Ana <anya.phakadze@yandex.ru>
2024-12-04 02:34:53 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,36 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(data, deleteCard) {   // Функция для создания элемента карточки
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); // клонирование шаблона
+    
+    
+    cardElement.querySelector('.card__image').src = data.link;  // Заполняем элемент данными
+    cardElement.querySelector('.card__image').alt = data.name;
+    cardElement.querySelector('.card__title').textContent = data.name;
+    
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function() { //  обработчик клика по иконке
+    deleteCard(cardElement);
+    });
+    
+    return cardElement;
+}
+    
+    
+function deleteCard(cardElement) { // Функция удаления карточки 
+    cardElement.remove();
+}
+    
+
+const placesList = document.querySelector('.places__list'); // Выводим все карточки из массива на страницу в элемент .places__list
+
+initialCards.forEach(function(card) {
+    const newCard = createCard(card, deleteCard);
+    placesList.append(newCard);
+});
+
+
+
